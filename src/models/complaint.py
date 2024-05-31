d63d93e77d29998a03684b187abab3db6c746425
PIHU <dim.pigulsky@gmail.com>
2024-05-31 10:17:07 +0200

    Complaint

--- src/models/complaint.py
+++ src/models/complaint.py
@@ -1,2 +1,20 @@
+import sqlalchemy as sa
+
 class Complaint:
-    pass
+    tablename = "complaints"
+    id = sa.Column('id', sa.Integer, primary_key=True)
+    employee_id = sa.Column('employee_id type', sa.Integer)
+    service_id = sa.relationship("services", back_populates="id")
+    open_date = sa.Column('open_date', sa.Date)
+    closure_date = sa.Column('closure_date', sa.Date)
+    description = sa.Column('description', sa.String(25))
+    cost = sa.Column('cost', sa.DECIMAL(8, 2))
+
+    def init(self, id, employee_id, service_id, open_date, closure_date, description, cost):
+        self.id = id
+        self.employee_id = employee_id
+        self.service_id = service_id
+        self.open_date = open_date
+        self.closure_date = closure_date
+        self.description = description
+        self.cost = cost
