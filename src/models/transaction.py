210b435dfe0b64bf3e2a002ff977b39e1b0b2e92
Dzmitry Pihulski <dim.pigulsky@gmail.com>
2024-06-04 15:21:21 +0200

    Transactions

--- src/models/transaction.py
+++ src/models/transaction.py
@@ -3,31 +3,31 @@ import sqlalchemy as sa
 from src.models.base import Base
 
 
-class TransactionTypes(sa.enum.Enum):
+class TransactionMethod(sa.enum.Enum):
     cash = 1
     card = 2
 
 
 class TransactionTypes(sa.enum.Enum):
-    cash = 1
-    card = 2
+    income = 1
+    cost = 2
 
 
 class Transaction(Base):
     __tablename__ = "transactions"
     id = sa.Column('id', sa.Integer, primary_key=True)
-    transaction_type = sa.Column('transaction type', sa.Enum(TransactionTypes))
-    id_sender = sa.orm.mapped_column(sa.ForeignKey("customer.id"))
+    transaction_type = sa.Column('transaction type', sa.Enum(TransactionMethod))
+    other_party = sa.orm.mapped_column(sa.ForeignKey("customer.id"))
     date = sa.Column('data', sa.Date)
-    topic = sa.Column('topic', sa.Enum())
+    topic = sa.Column('topic', sa.Enum(TransactionTypes))
     value = sa.Column('value', sa.DECIMAL(8, 2))
 
     sender = sa.orm.relationship('Customer')
 
-    def init(self, id, transaction_type, id_sender, date, topic, value):
+    def init(self, id, transaction_type, other_party, date, topic, value):
         self.id = id
         self.transaction_type = transaction_type
-        self.id_sender = id_sender
+        self.other_party = other_party
         self.date = date
         self.topic = topic
         self.value = value
