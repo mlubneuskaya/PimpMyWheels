df79b46b96966c42787f34bd4663b806e4151d3d
Dzmitry Pihulski <dim.pigulsky@gmail.com>
2024-06-09 19:40:38 +0200

    equipment corrected

--- src/models/equipment.py
+++ src/models/equipment.py
@@ -6,17 +6,16 @@ from src.models.base import Base
 
 class Equipment(Base):
     __tablename__ = "equipment"
-
     id = sa.Column(sa.Integer, autoincrement=True, primary_key=True, comment="Id wyposażenia")
     name = sa.Column(sa.String(255), nullable=False, comment="Nazwa")
     type = sa.Column(sa.String(50), nullable=False, comment="Typ")
     number_in_stock = sa.Column(sa.Integer, nullable=False, comment="Ilość na stanie")
-    facility_id = sa.Column(sa.Integer, sa.ForeignKey("facilities.id"), nullable=False, comment="Id placówki")
+    workshop_id = sa.Column(sa.Integer, sa.ForeignKey("workshops.id"), nullable=False, comment="Id placówki")
 
-    facility = relationship("Facility")
+    workshop = relationship("Workshop")
 
-    def __init__(self, name, type, number_in_stock, facility_id):
+    def __init__(self, name, type, number_in_stock, workshop_id):
         self.name = name
         self.type = type
         self.number_in_stock = number_in_stock
-        self.facility_id = facility_id
+        self.workshop_id = workshop_id
