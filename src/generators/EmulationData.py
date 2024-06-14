66bd51e442ca5b153ffc30bbe3fa69cd351b8c69
mlubneuskaya <mlubnewskaja@gmail.com>
2024-06-14 16:11:36 +0200

    emulation data class

---
+++ src/generators/EmulationData.py
@@ -0,0 +1,7 @@
+class EmulationData:
+    def __init__(self):
+        active_customers = []
+        inactive_customers = []
+        employees = []
+        inactive_employees = []
+        workshops = []
