3d50c9930549671ad1574a7c43c62e045c2b7467
Dzmitry Pihulski <dim.pigulsky@gmail.com>
2024-06-14 12:48:20 +0200

    create_complaint

--- src/emulation/customer_decision_maker.py
+++ src/emulation/customer_decision_maker.py
@@ -8,6 +8,13 @@ def create_order(customer, employee,  day):
     return Services(customer=customer, employee=employee, transaction=transaction)
 
 
-def create_complaint(customer, day):
-    customer.last_active = day
-    return Complaint()
+def create_complaint(customer, open_date, employee, service, closure_date, description, cost):
+    customer.last_active = open_date
+    return Complaint(
+        employee,
+        service,
+        open_date,
+        closure_date,
+        description,
+        cost
+    )
